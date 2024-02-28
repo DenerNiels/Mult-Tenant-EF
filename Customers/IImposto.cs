2495bcb33baf9262ee1c80ee59403262d91602e8
Dener Niels <ksin_niels@outlook.com>
2024-02-28 07:14:43 +0000

    .

--- Customers/IImposto.cs
+++ Customers/IImposto.cs
@@ -1,5 +1,23 @@
 namespace MultiRegras.Customers
 {
+
+    //public abstract class Imposto
+    //{
+    //    public virtual decimal CalcularICMS(decimal valor)
+    //    {
+    //        return valor * 2;
+    //    }
+    //}
+
+    //public class ImpostoBalta : Imposto
+    //{
+    //    public override decimal CalcularICMS(decimal valor)
+    //    {
+    //        return valor * 3;
+    //    }
+    //}
+
+
     public interface IImposto
     {
         public decimal CalcularICMS(decimal valor);
